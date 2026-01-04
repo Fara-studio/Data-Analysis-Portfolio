66dcc2270e32f76a05a7978531c433d713e18633
Fara-studio <youngnewman0@gmail.com>
2026-01-04 19:11:42 +0500

    Add sales analysis queries to 03_sales_analysis.sql
    This SQL file contains various queries for sales analysis, including total sales, orders count, average order value, monthly sales, top products, category sales, sub-category sales, and sales per city.

---
+++ SQL/03_sales_analysis.sql
@@ -0,0 +1,53 @@
+-- 1. Total sales
+SELECT SUM(Sales) AS total_sales
+FROM superstore;
+
+-- 2. Orders count
+SELECT COUNT(DISTINCT [Order_ID]) AS orders_count
+FROM superstore;
+
+-- 3. Average order 
+SELECT SUM(SALES) / COUNT(DISTINCT[Order_ID]) AS avg_order_value
+FROM superstore;
+
+-- 4. Month sales
+SELECT
+	YEAR([Order_Date]) AS year,
+	MONTH([Order_Date]) AS month,
+	SUM(Sales) AS sales
+FROM superstore
+GROUP BY YEAR([Order_Date]), MONTH([Order_Date])
+ORDER BY year, month;
+
+-- 5. Top 5 product`s
+SELECT TOP 5 
+	[Product_Name],
+	SUM(Sales) AS sales
+FROM superstore
+GROUP BY [Product_Name]
+ORDER BY sales DESC;
+
+-- 6. Category sales
+SELECT
+	Category,
+	SUM(Sales) as sales
+FROM superstore
+GROUP BY Category
+ORDER BY sales DESC;
+
+-- 7. Sub-category sales(for details)
+SELECT 
+	Category,
+	[Sub_Category],
+	SUM(Sales) as sales
+FROM superstore
+GROUP BY Category, [Sub_Category]
+ORDER BY Category, sales DESC;
+
+-- 8. Sales per Cities
+SELECT TOP 10
+	City,
+	SUM(Sales) AS sales
+FROM superstore
+GROUP BY City
+ORDER BY sales DESC;
